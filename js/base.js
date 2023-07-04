b9257d6120252e6efe902d926a80947e3f9be28f
ottomenku <ottomenkue@gmail.com>
2023-07-04 13:04:05 +0200

    efwserqwer

--- js/base.js
+++ js/base.js
@@ -2,20 +2,39 @@ mat = 'ms';
 cat = 'rag';
 subcat = 'rag';
 fomenulist = [];
+oldalmenutomb = [];
 
-
-function fomenutomb() {
+function almenutomb() {
     rekord.forEach(myFunction);
     //document.getElementById("demo").innerHTML = text;
 
     function myFunction(item, index) {
-        if (!fomenulist.includes(item[0])) {
-            fomenulist.push(item[0]);
+        if (mat == item[0])) {
+        if (!oldalmenutomb.includes(item[1])) {
+            oldalmenutomb.push(item[1]);
+        } else {
+            if (!oldalmenutomb[item[1]].includes(item[2])) {
+                oldalmenutomb[item[1]].push(item[2]);
+            }
         }
     }
 
 }
 
+function fomenu() {
+    $("#fomenu").html('');
+
+    fomenulist.forEach(myFunction);
+
+    function myFunction(item, index) {
+        menuitem = $("#menu1").clone()
+        $(menuitem).text(linktext[item]);
+        $(menuitem).attr("id", item);
+        $(menuitem).attr("onclick", "oldalnuactiv(this); listaz(" + item + "); ");
+        $("#fomenu").append(menuitem);
+
+    }
+}
 
 function fomenutomb() {
     rekord.forEach(myFunction);
@@ -38,7 +57,7 @@ function fomenu() {
         menuitem = $("#menu1").clone()
         $(menuitem).text(linktext[item]);
         $(menuitem).attr("id", item);
-        $(menuitem).attr("onclick", "listaz(" + item + "); fomenuactiv(this);");
+        $(menuitem).attr("onclick", "fomenuactiv(this); listaz(" + item + "); ");
         $("#fomenu").append(menuitem);
 
     }
@@ -60,9 +79,15 @@ function kiir() {
 function fomenuactiv(e) {
     $(".fomenu").removeClass("active");
     $(e).addClass("active");
+    mat = e.id;
+    // alert(mat);
 }
 
 function oldalmenuactiv(e) {
     $(".oldalmenu").removeClass("active");
     $(e).addClass("active");
+    subcat = e.id;
+    cat = $(e).parent().attr('id');
+    // alert(cat);
+    // alert(subcat);
 }
