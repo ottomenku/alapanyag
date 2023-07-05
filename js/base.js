13e1eabf48dfab870ac2e5576a7bd058f709a190
ottomenku <ottomenkue@gmail.com>
2023-07-05 12:54:31 +0200

    gfhfghgf

--- js/base.js
+++ js/base.js
@@ -4,47 +4,86 @@ subcat = 'rag';
 fomenulist = [];
 oldalmenuobj = {};
 //["ms", "rag", "sok", "kep", "note"],
+function inobj(obj, keres) {
+    var found = false;
+    for (var i = 0; i < obj.length; i++) {
+        if (vendors[i].Name == keres) {
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
 function oldalmenutomb() {
     rekord.forEach(myFunction);
     //document.getElementById("demo").innerHTML = text;
 
     function myFunction(item, index) {
+        kat = item[1];
+        menu = item[2];
         if (mat == item[0]) {
-            if (!oldalmenuobj.includes(item[1])) {
-                oldalmenuobj.push(item[1]);
+            if (!inobj(oldalmenuobj, kat)) {
+                oldalmenuobj[kat] = []
+                    // oldalmenuobj.push({ kat: [] });
             } else {
-                if (!oldalmenuobj[item[1]].includes(item[2])) {
-                    oldalmenuobj[item[1]].push(item[2]);
+                if (!oldalmenuobj[kat].includes(menu)) {
+                    oldalmenuobj[kat].push(menu);
                 }
             }
         }
 
     }
 }
+
 function oldalmenu() {
-   oldalmenuobj.forEach(myFunction);
+
+
+    Object.entries(oldalmenuobj).forEach(entry => {
+        const [key, value] = entry;
+        // console.log(key, value);
+        blockindex = "oldalmenublock" + key;
+        block = $("#oldalmenublock").clone();
+        $(block).attr("id", blockindex);
+        $("#oldalmenu").append(block);
+
+
+        menukat = $("#oldalmenukat").clone();
+        $(menukat).attr("id", key);
+        $(menukat).text(linktext[key]);
+        $("#oldalmenublock" + key).append(menuitem);
+
+    });
+
+}
+/*
+
+function oldalmenu() {
+    oldalmenuobj.forEach(myFunction);
 
     function myFunction(item, index) {
-        blockindex="oldalmenublock"+index;
+        blockindex = "oldalmenublock" + index;
         block = $("#oldalmenublock").clone();
-        $(block).attr("id","oldalmenublock"+index );
-      $("#oldalmenu").append(menuitem);
-        menukat=$("#oldalmenukat").clone();
+        $(block).attr("id", "oldalmenublock" + index);
+        $("#oldalmenu").append(menuitem);
+        menukat = $("#oldalmenukat").clone();
         $(menukat).attr("id", index);
-         $(menukat).text(linktext[index]);
-        $("#oldalmenublock"+ index).append(menuitem); 
-       item.forEach(myFunction2);
-    function myFunction2(item, index) {
-        menuitem=$("#oldalmenu").clone()
-        $(menuitem).attr("onclick", "oldalmenuactiv(this); listaz(" + item + "); ");
-         $(menuitem).attr("id", item)
-         $(menuitem).text(linktext[item]);
-        $("#"+blockindex).append(menuitem);
-    }
+        $(menukat).text(linktext[index]);
+        $("#oldalmenublock" + index).append(menuitem);
+        item.forEach(myFunction2);
+
+        function myFunction2(item, index) {
+            menuitem = $("#oldalmenu").clone()
+            $(menuitem).attr("onclick", "oldalmenuactiv(this); listaz(" + item + "); ");
+            $(menuitem).attr("id", item)
+            $(menuitem).text(linktext[item]);
+            $("#" + blockindex).append(menuitem);
+        }
     }
 }
+*/
 function fomenu() {
-   // $("#fomenu").html('');
+    // $("#fomenu").html('');
 
     fomenulist.forEach(myFunction);
 
@@ -52,7 +91,7 @@ function fomenu() {
         menuitem = $("#menu").clone()
         $(menuitem).text(linktext[item]);
         $(menuitem).attr("id", item);
-        $(menuitem).attr("onclick", "oldalnuactiv(this); listaz(" + item + "); ");
+        $(menuitem).attr("onclick", "fomenuactiv(this); listaz(" + item + "); ");
         $("#fomenu").append(menuitem);
 
     }
