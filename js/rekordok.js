7378e6c9abce5b16b3feae6a6ac76a367d21f480
Ménkű Ottó <ottomenku@gmail.com>
2023-07-05 22:22:04 +0200

    Update rekordok.jsfff

--- js/rekordok.js
+++ js/rekordok.js
@@ -2,19 +2,24 @@ const linktext = {
     ms: "Mastershipper",
     db: "Displaybox",
     ins: "Insert-Tray",
-    mas: "Egyéb";
-    sok: "Sok";
-    keves: "Kevés";
-    rag: "Ragasztás";
-    nyom: "Nyomtatás";
+    mas: "Egyéb",
+    sok: "Sok",
+    keves: "Kevés",
+    rag: "Ragasztás",
+    nyom: "Nyomtatás",
     crease: "Creaseline"
 
 };
 
 rekord = [
     ["ms", "rag", "sok", "kep", "note"],
+    ["ms", "nyom", "sok", "kep", "note"],
+    ["ms", "rag", "keves", "kep", "note"],
+    ["ms", "crease", "keves", "kep", "note"],
+    ["ms", "rag", "pozicio", "kep", "note"], 
+    ["ms", "rag", "keves", "kep", "note"],
     ["db", "nyom", "sok", "kep", "note"],
-    ["ins", "rag", "keves", "kep", "note"],
+    ["ins", "rag", "keves", "kep", "note"],       
     ["db", "crease", "keves", "kep", "note"],
     ["db", "rag", "pozicio", "kep", "note"],
 ];
