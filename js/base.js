f2c6a0a7ae6f92399e961590375c7edd48282b7b
ottomenku <ottomenkue@gmail.com>
2023-07-06 12:52:10 +0200

    fdhdf

--- js/base.js
+++ js/base.js
@@ -15,33 +15,71 @@ function inobj(obj, keres) {
     return found;
 }
 
+function itemlistaz(e) {
+    id = e.id;
+    const myArray = id.split("_");
+    let kat0 = myArray[0];
+    let subkat0 = myArray[1];
+    $("#content").html("");
+    rekord.forEach(itemlista);
+
+    function itemlista(item, index) {
+        // console.log('fff');
+        note = item[4];
+        src = item[3];
+        subkat = item[2];
+        kat = item[1];
+        mat = item[0];
+
+        itemblock = $("#itemblock").clone();
+        $(itemblock).attr("id", 'itemblock' + index);
+
+        imageblock = $("#imageblock").clone();
+        $(imageblock).attr("id", 'imageblock' + index);
+        $(imageblock).html(' <img id="image" src=" image/doboz.jpg " width="90% ">');
+
+        itemnote = $("#itemnote").clone();
+        $(itemnote).attr("id", "itemnote" + index);
+        // $(menukat).text(linktext[note]);
+        $(itemnote).html(note);
+
+        $(itemblock).append(imageblock);
+        $(itemblock).append(itemnote);
+        $("#content").append(itemblock);
+
+
+    }
+}
+
 function oldalmenu() {
     $("#oldalmenu").html('');
     rekord.forEach(oldalmenus);
 
-}
+    function oldalmenus(item, index) {
 
-function oldalmenus(item, index) {
-
-    kat = item[1];
-    menuitem = item[2];
+        kat = item[1];
+        menuitem = item[2];
 
 
-    if (mat == item[0]) {
+        if (mat == item[0]) {
 
-        if (!$('#block' + kat).length) {
-            oldalmenublock(kat);
+            if (!$('#block' + kat).length) {
+                oldalmenublock(kat);
+            }
+
+            if (!$('#kat' + kat).length) {
+                oldalmenukat(kat);
+            }
+            if (!$('#' + kat + '_' + menuitem).length) {
+                oldalmenuitem(kat, menuitem);
+            }
+
         }
-
-        if (!$('#kat' + kat).length) {
-            oldalmenukat(kat);
-        }
-        if (!$('#' + kat + menuitem).length) {
-            oldalmenuitem(kat, menuitem);
-        }
-
     }
+
 }
+
+
 
 
 function oldalmenublock(kat) {
@@ -63,7 +101,7 @@ function oldalmenukat(kat) {
 function oldalmenuitem(kat, itemid) {
 
     item = $("#oldalmenuitem").clone();
-    $(item).attr("id", kat + itemid);
+    $(item).attr("id", kat + "_" + itemid);
     $(item).text(linktext[itemid]);
     $("#block" + kat).append(item);
 }
