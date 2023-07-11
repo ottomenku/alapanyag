4c0b593e3e88f522f4fb7d71862ff406a5874301
Ménkű Ottó <ottomenku@gmail.com>
2023-07-11 07:11:22 +0200

    Create formrekordok.js

---
+++ js/formrekordok.js
@@ -0,0 +1,7 @@
+let lang=1;  //1 magyar, 2angol
+let mattype=[
+  ["db","Dísplaybox","Dísplaybox"],
+   ["ms","Mastershipper","Mastershipper"],
+    ["tray","Tray_insert","Tray_insert"],
+   ["mas","Egyéb","Other"],
+];
